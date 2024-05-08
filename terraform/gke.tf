f0bf2c12fd83c94ece1fbe2715aec3ef45de924e
Nagesha ks <109476085+NAGESHGOWDA1993@users.noreply.github.com>
2024-05-08 13:58:08 +0100

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -7,7 +7,7 @@ resource "google_container_cluster" "my_cluster" {
   name     = var.cluster_name
   location = var.region
 
-  remove_default_node_pool = false
+  remove_default_node_pool = true
 
   initial_node_count  = 1
   deletion_protection = false
